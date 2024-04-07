ba866e21eadfed37938a5c20dc3d22f6e99ef97b
VINESH08 <vineshraghu0809@gmail.com>
2024-04-07 17:31:06 +0530

    new  file

---
+++ Graph/BellmanFord.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+struct node
+{
+	int from;
+	int to;
+	int weight;
+	node *next;
+};
+struct vnode
+{
+	string name;
+	int d;
+	int pi;
+};
+class graph
+{
+	int n, m;
+	vnode vertex[10];
+	node *elist;
+
+public:
+	void createGraph()
+	{
+		// string x = "4";
+		// int y = stoi(x);
+		// cout << y << endl;
+		cout << "Enter number of vertices: ";
+		cin >> n;
+
+		for (int i = 1; i <= n; i++)
+		{
+			cout << "Enter name of the vertex - " << i << ": ";
+			cin >> vertex[i].name;
+		}
+		cout << "Enter number of edges: ";
+		cin >> m;
+		elist = NULL;
+		node *last = NULL;
+		for (int j = 0; j < m; j++)
+		{
+			node *t = new node;
+			cout << "Enter index of from vertex: ";
+			cin >> t->from;
+			cout << "Enter index of to vertex: ";
+			cin >> t->to;
+			cout << "Enter edge weight: ";
+			cin >> t->weight;
+			t->next = NULL;
+			if (elist == NULL)
+				elist = last = t;
+			else
+				last->next = t;
+			last = t;
+		}
+	}
+	void INITIALIZE_SINGLE_SOURCE(int s)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			vertex[i].d = 9999;
+			vertex[i].pi = -1;
+		}
+		vertex[s].d = 0;
+	}
+
+	void RELAX(int u, int v, int w)
+	{
+		if (vertex[v].d > vertex[u].d + w)
+		{
+			vertex[v].d = vertex[u].d + w;
+			vertex[v].pi = u;
+		}
+	}
+	bool BellmanFord(int s)
+	{
+		INITIALIZE_SINGLE_SOURCE(s);
+
+		for (int i = 1; i < m; i++)
+		{
+			node *t = elist;
+			while (t != NULL)
+			{
+				RELAX(t->from, t->to, t->weight);
+				t = t->next;
+			}
+		}
+		node *t = elist;
+		// Check for any negative cycle....
+		while (t != NULL)
+		{
+			if (vertex[t->to].d > vertex[t->from].d + t->weight)
+				return false;
+		}
+		return true;
+	}
+};
+int main()
+{
+	graph obj;
+	obj.createGraph();
+	int st;
+	cout << "Enter the start vertex" << endl;
+	cin >> st;
+	bool res = obj.BellmanFord(st);
+	if (res)
+		cout << "No negative cycle is present in the graph" << endl;
+	else
+		cout << "Negative cycle is present in the given graph" << endl;
+}
---
+++ Graph/FloyadWarshall.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+class FloydWarshall
+{
+private:
+    int **pi;
+    int **d;
+    int **w;
+
+public:
+    FloydWarshall(int size)
+    {
+        pi = new int *[size];
+        for (int i = 0; i < size; i++)
+            pi[i] = new int[size];
+        d = new int *[size];
+        for (int i = 0; i < size; i++)
+            d[i] = new int[size];
+        w = new int *[size];
+        for (int i = 0; i < size; i++)
+            w[i] = new int[size];
+    }
+    void intialize(int wrows)
+    {
+        int n = wrows;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                d[i][j] = w[i][j];
+                if (i == j || w[i][j] == 999)
+                    pi[i][j] = -1;
+                else
+                    pi[i][j] = i + 1;
+            }
+        }
+    }
+    void floydwarshall(int drows)
+    {
+        int n = drows;
+        for (int k = 0; k < n; k++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (pi[i][k] != -1 && pi[k][j] != -1 && d[i][j] > (d[i][k] + d[k][j]))
+                    {
+                        d[i][j] = d[i][k] + d[k][j];
+                        pi[i][j] = pi[k][j];
+                    }
+                }
+            }
+        }
+    }
+    void input(int size)
+    {
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                cout << "Enter element:" << i + 1 << "," << j + 1 << "Element" << endl;
+                cin >> w[i][j];
+            }
+        }
+    }
+    void output(int size)
+    {
+        cout << "MATRIX D" << endl;
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                cout << d[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        cout << "MATRIX pi" << endl;
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                cout << pi[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+int main()
+{
+    int size;
+    cout << "Enter the number of edges" << endl;
+    cin >> size;
+    FloydWarshall obj(size);
+    obj.input(size);
+    obj.intialize(size);
+    obj.floydwarshall(size);
+    obj.output(size);
+}
